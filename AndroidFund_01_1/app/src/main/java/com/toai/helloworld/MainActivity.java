ce3ddb50006c0cd2952d9c5c53ffc8119d20c9c6
toai-php <56766134+toai-php@users.noreply.github.com>
2021-09-29 00:03:36 +0700

    - add Codelab 1.1

---
+++ AndroidFund_01_1/app/src/main/java/com/toai/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.toai.helloworld;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "HelloWorld");
+        Log.i("MainActivity", "MainActivity layout is complete");
+    }
+}
